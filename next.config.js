c5a86d43f389d5e496a1af679375d1f4546e651e
404kidwiz <404kidwiz@gmail.com>
2026-04-01 10:33:36 -0400

    security: remove unused OPENAI_API_KEY exposure from next config

--- next.config.js
+++ next.config.js
@@ -6,9 +6,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  env: {
-    NEXT_PUBLIC_OPENAI_API_KEY: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
-  },
 };
 
 module.exports = nextConfig;
